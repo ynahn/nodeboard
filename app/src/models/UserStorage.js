b82bcd846ab395762b61a4aeccbc1e127872c6d2
ynahn <ynahn@goldenplanet.co.kr>
2021-03-18 16:32:22 +0900

    모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,15 +1,23 @@
 "use strict";
 
+const fs = require("fs").promises;
+
 class UserStorage {
   //"#" 은닉화 -> 외부에서 불러올수 없음.
-  static #users = {
-    id: ["안유니", "임어진", "황혜정"],
-    psword: ["0124", "0204", "1030"],
-    names: ["유니주정뱅이꾸꾸", "임어진", "황혜정"],
-  };
+  static #getUserInfo(data, id) {
+    const users = JSON.parse(data);
+    const idx = users.id.indexOf(id);
+    const usersKeys = Object.keys(users); // => [id, psword, names]
+    const userInfo = usersKeys.reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
+    // console.log(userInfo);
+    return userInfo;
+  }
 
   static getUsers(...fields) {
-    const users = this.#users;
+    // const users = this.#users;
     const newUsers = fields.reduce((newUsers, field) => {
       if (users.hasOwnProperty(field)) {
         newUsers[field] = users[field];
@@ -21,18 +29,16 @@ class UserStorage {
   }
 
   static getUserInfo(id) {
-    const users = this.#users;
-    const idx = users.id.indexOf(id);
-    const usersKeys = Object.keys(users); // => [id, psword, names]
-    const userInfo = usersKeys.reduce((newUser, info) => {
-      newUser[info] = users[info][idx];
-      return newUser;
-    }, {});
-    return userInfo;
+    return fs
+      .readFile("./src/databases/users.json")
+      .then((data) => {
+        return this.#getUserInfo(data, id);
+      })
+      .catch(console.error);
   }
 
   static save(userInfo) {
-    const users = this.#users;
+    // const users = this.#users;
 
     users.id.push(userInfo.id);
     users.names.push(userInfo.name);
